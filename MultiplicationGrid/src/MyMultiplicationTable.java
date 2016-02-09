5e01db57fab4ff07d1b2859b3c6573184347365a
Ghebre <geberhane@gmail.com>
2016-02-08 20:30:11 -0800

    Multiplication Table
    Multiplication Table

---
+++ MultiplicationGrid/src/MyMultiplicationTable.java
@@ -0,0 +1,18 @@
+
+public class MyMultiplicationTable {
+
+	public static void main(String[] args) {
+		 
+		for(int i=1; i <= 12; i++){
+			 System.out.print(i + "\t");
+			for(int j=1; j <= 12; j++){ 
+				System.out.print(i*j + "\t");
+			
+			}
+			System.out.println();
+			}
+		}
+
+	}
+
+ 
